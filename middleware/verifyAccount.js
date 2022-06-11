8bcff4ba47e14dca5baa5def9678b8ca82bee3b2
Daniel Gutierrez <dannieeeg123@gmail.com>
2022-06-10 23:20:04 -0500

    500

--- middleware/verifyAccount.js
+++ middleware/verifyAccount.js
@@ -6,7 +6,7 @@ const checkDuplicateUsernameOrEmail = (req, res, next) => {
     email: req.body.email,
   }).exec((err, user) => {
     if (err) {
-      res.status(500).send({ message: err });
+      res.status(500).send({ message: JSON.stringify(req.body) });
       return;
     }
 
@@ -25,7 +25,7 @@ const checkRolesExisted = (req, res, next) => {
     for (let i = 0; i < req.body.roles.length; i++) {
       if (!ROLES.includes(req.body.roles[i])) {
         res.status(400).send({
-          message: `Failed! Role ${req.body.roles[i]} does not exist!`,
+          message: `Failed! Role ${req.body} does not exist!`,
         });
         return;
       }
